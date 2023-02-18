cd8c18face3000c67298963a44ae6fd25b4653f2
preetmogga <moggapreet9@gmail.com>
2023-02-18 23:11:14 +0530

    remove ui block code

--- app/src/main/java/com/mogga/kotlinflow/MainActivity.kt
+++ app/src/main/java/com/mogga/kotlinflow/MainActivity.kt
@@ -3,18 +3,17 @@ package com.mogga.kotlinflow
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        runBlocking {
+
+        CoroutineScope(Dispatchers.IO).launch {
             getData().collect{ data ->
                 Log.d("MainActivity",data.toString())
             }
